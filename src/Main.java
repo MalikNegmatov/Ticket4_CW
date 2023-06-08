d5797b1c51541bfd0d7c11ff72c3b73283829ce2
MalikNegmatov <maliknegmatov@gmail.com>
2023-06-08 10:24:47 +0300

    Добавили метод объединения строк в массиве

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,21 @@ public class Main {
         // Инициализируем тестовый архив строк
         String[] testArray = {"ясень", "тополь", "береза", "сосна", "ясень", "дуб", "береза", "Ясень", "тополь", "сосна", "сосна",};
         System.out.println(Arrays.toString(testArray));
+        System.out.println(allOneString(testArray));
+    }
+
+    /**
+     * Метод объединения всех строк в одну в порядке следования в массиве
+     * @param arr -- передаваемый в метод массив строк
+     * @return -- возвращает переменную строку объединением всех строк массива
+     */
+    public static String allOneString(String[] arr) {
+        // используем StringBuilder, чтобы не плодить экземпляры класса String
+        StringBuilder result = new StringBuilder();
+        for (String w : arr ) {
+            if ( w != null ) result.append(w);
+        }
+        return String.valueOf(result);
+        // В условии задачи нет требования к добавлению разделителя между строками
     }
 }
